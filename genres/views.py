883647a8678d3de554ddc285e258868f2dda5d93
arecomarcelo <marcelo.areco@hotmail.com>
2024-10-23 19:41:43 -0300

    Criação da Classe Permissao Genres

--- genres/views.py
+++ genres/views.py
@@ -1,17 +1,18 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from .serializers import GenreSerializer
 
-from genres.models import Genre
+from .serializers import GenreSerializer
+from .models import Genre
+from .permissions import GenrePermissionClass
 
 #Lista Todos ou Adiciona Generos
 class GenreCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GenrePermissionClass,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 #Lista, Altera ou Exclui um Genero Específico
 class GenreRetriveCreateListView (generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GenrePermissionClass,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
